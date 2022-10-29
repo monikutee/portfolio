df13cdbb480033759f4b1174855e2b2b8a298ea4
monikutee <petrulevicmonika@gmail.com>
2022-10-30 00:27:19 +0300

    mobile scroll translation

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,15 @@
+const desktopHeader = 85;
+const mobileHeader = 280;
+
 export const scrollWithOffset = (el: any) => {
   const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-  const yOffset = -85;
-  window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+  if (viewportWidth > 991) {
+    window.scrollTo({ top: yCoordinate - desktopHeader, behavior: "smooth" });
+  } else {
+    window.scrollTo({ top: el.offsetTop - mobileHeader, behavior: "smooth" });
+  }
 };
